461555dc20f7520f43f6846c2c71aa62ea160dd6
Sean Bruen <kyau@kyau.net>
2017-08-05 02:17:44 -0700

    Fish Motd * Added tty to right corner * Commented out environmental variables for now

--- config/fish/motd.fish
+++ config/fish/motd.fish
@@ -2,15 +2,33 @@
 
 set ANSI $HOME/bin/(hostname -s).ans
 
+function get_padding
+	set -l space ""
+	for i in (seq 1 $argv[1])
+		set space " "$space
+	end
+	printf $space
+end
+
+function remove_color
+	printf $argv | perl -pe 's/\x1b.*?[mGKH]//g'
+end
+
 function warning
-	printf " \x1b[38;5;124m\u2026 here be dragons\x1b[0m\n"
+  set -l cols (tput cols)
+	set -l left " . here be dragons"
+	set -l lastlogin (last -2 -R  $USER | head -2 | cut -c 20- | sed -n 2p | sed -r 's/(.*) -.*/\1/')
+  set -l tty (tty | sed -r 's/\/dev\/(.*)/\1/' | sed -r 's/\///')
+  set -l right ".tty/$tty"
+  set -l padding (get_padding (math $cols - (echo "$left$right" | string length)))
+	printf "\x1b[38;5;124m \u2026 here be dragons\x1b[0m$padding\x1b[38;5;244m\$tty\x1b[0m\x1b[38;5;240m/$tty\n"
 end
 
 function getdistro
 	set -l text (grep "PRETTY_NAME" /etc/*release | sed -r 's/.*:PRETTY_NAME="(.*)"/\1/')
 	switch $text
 		case 'Arch*'
-			printf " \x1b[38;5;32m \uf300 \x1b[38;5;252march\x1b[38;5;32mlinux\e[0m"
+			printf " \x1b[38;5;32m \uf300 \x1b[38;5;252march\x1b[38;5;32mlinux\x1b[0m"
 		case '*'
 			printf " $text"
 	end
@@ -23,7 +41,7 @@ function network
 	set -l nic (/bin/ls /sys/class/net | head -1)
 	set -l iplist (ip -4 addr show $nic | grep -oP "(?<=inet ).*(?=/)" | sed -e :a -e '$!N; s/\n/, /; ta')
 	set -l fullhost (hostname -f)
-	printf "    \e[38;5;244m$fullhost\e[0m \e[38;5;240m($iplist)\n"
+	printf "    \x1b[38;5;244m$fullhost\x1b[0m \x1b[38;5;240m($iplist)\n"
 end
 
 function environ
@@ -32,8 +50,8 @@ function environ
 	set -l page (echo "$PAGER" | sed -r 's/.*\/(.*)/\1/')
 	set -l ter (echo "$TERM" | sed -r 's/(.*)-.*/\1/')
 	set -l grp (groups | sed -r 's/ /,/g')
-	printf "    \e[38;5;244m\$user\e[0m\e[38;5;240m/$USER\e[0m   \e[38;5;244m\$groups\e[0m\e[38;5;240m/$grp\n"
-	printf "    \e[38;5;244m\$editor\e[0m\e[38;5;240m/$edit\e[0m  \e[38;5;244m\$pager\e[0m\e[38;5;240m/$page  \e[38;5;244m\$shell\e[0m\e[38;5;240m/$shel\e[0m  \e[38;5;244m\$term\e[0m\e[38;5;240m/$ter\e[0m\n"
+	printf "    \x1b[38;5;244m\$user\x1b[0m\x1b[38;5;240m/$USER\x1b[0m   \x1b[38;5;244m\$groups\x1b[0m\x1b[38;5;240m/$grp\n"
+	printf "    \x1b[38;5;244m\$editor\x1b[0m\x1b[38;5;240m/$edit\x1b[0m  \x1b[38;5;244m\$pager\x1b[0m\x1b[38;5;240m/$page  \x1b[38;5;244m\$shell\x1b[0m\x1b[38;5;240m/$shel\x1b[0m  \x1b[38;5;244m\$term\x1b[0m\x1b[38;5;240m/$ter\x1b[0m\n"
 end
 
 
@@ -42,6 +60,6 @@ cat $ANSI
 echo -e
 getdistro
 network
-environ
+#environ
 
 # vim: ts=2 sw=2 noet :
