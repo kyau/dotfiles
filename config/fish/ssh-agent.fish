6507123f316f9181af03a59ac2195906b56d727d
Sean Bruen <kyau@kyau.net>
2017-08-14 05:16:40 -0400

    Add missing file for SSH Agent

---
+++ config/fish/ssh-agent.fish
@@ -0,0 +1,18 @@
+# $Arch: ssh-agent.fish,v 1.000 2017/08/14 04:56:52 kyau Exp $
+
+function __ssh_agent_is_started -d "check if ssh agent is already started"
+	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+		source $SSH_ENV > /dev/null
+	end
+	if test -z "$SSH_AGENT_PID"
+		return 1
+	end
+	ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
+	return $status
+end
+
+function __ssh_agent_start -d "start a new ssh agent"
+	ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+	chmod 600 $SSH_ENV
+	source $SSH_ENV > /dev/null
+end
