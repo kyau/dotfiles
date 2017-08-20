e4c6b1480a3210ed7532e919e058fd2b1ae35633
Sean Bruen <kyau@kyau.net>
2017-08-20 19:21:14 -0400

    More BSD Changes * Trying to make everyting Linux/BSD agnostic

--- config/fish/functions/sysinfo.fish
+++ config/fish/functions/sysinfo.fish
@@ -1,22 +1,46 @@
 # $Arch: sysinfo.fish,v 1.010 2017/08/07 19:27:26 kyau Exp $
 
 function sysinfo
-	clear
 	source $HOME/.config/fish/motd.fish
-	set -l colors (xrdb -query | sed -n 's/.*color\([0-9]\)/\1/p' | sort -nu | cut -f2)
+	if test -f /usr/bin/xhost
+		if test (xhost)
+			set -l colors (xrdb -query | sed -n 's/.*color\([0-9]\)/\1/p' | sort -nu | cut -f2)
+		end
+	else
+		set -l count 1
+		for x in (seq 50)[30..37 40..47]
+			if test $x -gt 39
+				set colors[$count] "1;$x"
+			else
+				set colors[$count] "0;$x"
+			end
+			set count (math $count + 1)
+		end
+	end
 	set -l edit (echo "$EDITOR" | sed -r 's/.*\/(.*)/\1/')
 	set -l shel (echo "$SHELL" | sed -r 's/.*\/(.*)/\1/')
 	set -l page (echo "$PAGER" | sed -r 's/.*\/(.*)/\1/')
 	set -l ter (echo "$TERM" | sed -r 's/(.*)-.*/\1/')
 	set -l grp (groups | sed -r 's/ /,/g')
-	set -l cpu (cat /proc/cpuinfo | grep "model name" | head -1 | string replace -r 'model name\s+:' '')
-	set -l cpu (echo $cpu | string replace -r "\(R\)" '' | string replace -r "\(TM\)" '' | string replace -r ' CPU' '' | string trim -l)
-	set -l pci (lspci | grep ' VGA ' | cut -d" " -f 1)
-	set -l gpu (lspci -v -s $pci | head -1 | string replace -r ".*:" '' | string replace -r " Corporation" '' | string replace -r "\(.*" '' | string trim -l)
-	set -l ram (awk '/^MemTotal:/{print $2}' /proc/meminfo)
-	set -l ram (math $ram / 1024)
-	set -l swap (awk '/^SwapTotal:/{print $2}' /proc/meminfo)
-	set -l swap (math $swap / 1024)
+	set -l cpu
+	set -l gpu
+	set -l ram
+	set -l swap
+	if test $FISH_PLATFORM = "OpenBSD"
+		set cpu "Intel Xeon(R) E5-1620 v2 @ 3.70GHz"
+		set gpu "Matrox MGA G200eW WPCM450"
+		set ram "1024"
+		set swap "4096"
+	else
+		set cpu (cat /proc/cpuinfo | grep "model name" | head -1 | string replace -r 'model name\s+:' '')
+		set cpu (echo $cpu | string replace -r "\(R\)" '' | string replace -r "\(TM\)" '' | string replace -r ' CPU' '' | string trim -l)
+		set -l pci (lspci | grep ' VGA ' | cut -d" " -f 1)
+		set gpu (lspci -v -s $pci | head -1 | string replace -r ".*:" '' | string replace -r " Corporation" '' | string replace -r "\(.*" '' | string replace -r " Electronics Systems Ltd." '' | string trim -l)
+		set ram (awk '/^MemTotal:/{print $2}' /proc/meminfo)
+		set ram (math $ram / 1024)
+		set swap (awk '/^SwapTotal:/{print $2}' /proc/meminfo)
+		set swap (math $swap / 1024)
+	end
 	printf "\\n    \\x1b[38;5;244muser\\x1b[0m\\x1b[38;5;240m/%s\\x1b[0m   \\x1b[38;5;244mgroups\\x1b[0m\\x1b[38;5;240m/%s\\n" "$USER" "$grp"
 	printf "    \\x1b[38;5;244meditor\\x1b[0m\\x1b[38;5;240m/%s\\x1b[0m  \\x1b[38;5;244mpager\\x1b[0m\\x1b[38;5;240m/%s  \\x1b[38;5;244mshell\\x1b[0m\\x1b[38;5;240m/%s\\x1b[0m  \\x1b[38;5;244mterm\\x1b[0m\\x1b[38;5;240m/%s\\x1b[0m\\n\\n" "$edit" "$page" "$shel" "$ter"
 	printf "    \\x1b[38;5;244mcpu\\x1b[0m\\x1b[38;5;240m/%s\\x1b[0m\\n" "$cpu"
--- config/fish/motd.fish
+++ config/fish/motd.fish
@@ -1,4 +1,4 @@
-# $Arch: motd.fish,v 1.021 2017/08/07 19:26:29 kyau Exp $
+# $Arch: motd.fish,v 1.022 2017/08/20 16:37:46 kyau Exp $
 
 # ANSI
 set ANSI $HOME/dot/ansi/(hostname -s).ans
@@ -27,23 +27,24 @@ function warning
   set -l tty (tty | sed -r 's/\/dev\/(.*)/\1/' | sed -r 's/\///')
   set -l right ".tty/$tty"
   set -l padding (get_padding (math $cols - (echo "$left$right" | string length)))
-	printf " \x1b[38;5;124m \u2026 here be dragons\x1b[0m$padding\x1b[38;5;244m\$tty\x1b[0m\x1b[38;5;240m/$tty\n"
+	printf " \x1b[38;5;124m \u2026 here be dragons\x1b[0m$padding\x1b[38;5;242m\$tty\x1b[0m\x1b[38;5;240m/$tty\n"
 end
 # }}}
 # Distribution & Kernel Version {{{
 function getdistro
+	set -l rtext
 	set -l text (grep "PRETTY_NAME" /etc/*release | sed -r 's/.*:PRETTY_NAME="(.*)"/\1/' | head -1)
-	switch $text
-		case 'Arch*'
-			printf " \x1b[38;5;32m \uf300 \x1b[38;5;252march\x1b[38;5;32mlinux\x1b[0m"
-		case 'OpenBSD*'
-			printf "    \x1b[38;5;226mOpenBSD\x1b[0m"
-		case '*'
-			printf " $text"
-	end
 	set -l ver (uname -r | sed -r 's/(.*)-(.*)-.*/\1-\2/')
 	set -l kernel "\x1b[38;5;252m$ver\x1b[0m"
-	printf " $kernel\n"
+	switch $text
+		case 'Arch*'
+			printf " \\x1b[38;5;242m│\\x1b[38;5;237m░\\x1b[0m\\x1b[38;5;32m \\uf300 \\x1b[38;5;252march\\x1b[38;5;32mlinux\\x1b[0m $kernel                                 \\x1b[38;5;237m░░░·\\x1b[0m\\n"
+
+		case 'OpenBSD*'
+			printf " \\x1b[38;5;242m│\\x1b[38;5;237m░\\x1b[0m\\x1b[38;5;199m \\ue225 \\x1b[38;5;226mopenbsd\\x1b[0m $kernel                                        \\x1b[38;5;237m░░░·\\x1b[0m\\n"
+		case '*'
+			printf "$text $kernel"
+	end
 end
 # }}}
 # Hostname & IPs {{{
@@ -52,19 +53,22 @@ function network
 		set -l nic (/bin/ls /sys/class/net | head -1)
 		set -l iplist (ip -4 addr show $nic | grep -oP "(?<=inet ).*(?=/)" | sed -e :a -e '$!N; s/\n/, /; ta')
 		set -l fullhost (hostname -f)
-		printf "    \x1b[38;5;244m$fullhost\x1b[0m \x1b[38;5;240m($iplist)\n"
+		printf " \\x1b[38;5;242m│\\x1b[38;5;237m░\\x1b[0m   \x1b[38;5;242m$fullhost\x1b[0m \x1b[38;5;240m($iplist)\n"
 	else
 		set -l iplist (cat /etc/systemip)
 		set -l fullhost (hostname)
-		printf "    \x1b[38;5;244m$fullhost\x1b[0m \x1b[38;5;240m($iplist)\n"
+		printf " \\x1b[38;5;242m│\\x1b[38;5;237m░\\x1b[0m   \x1b[38;5;242m$fullhost\x1b[0m \x1b[38;5;240m%-33s \\x1b[38;5;237m░\\x1b[38;5;242m:\\x1b[0m\\n" "($iplist)"
 	end
 end
 # }}}
 
 warning
 cat $ANSI
-echo -e
+printf " \\x1b[38;5;255m┌\\x1b[38;5;242m────\\x1b[38;5;235m─·\\x1b[38;5;242m─\x1b[38;5;235m─·─────────────────────────────────────────────────┐\\n"
 getdistro
 network
+printf " \\x1b[38;5;235m·\\x1b[38;5;237m░\\x1b[38;5;233m── \\x1b[38;5;235m· \\x1b[38;5;242m· \\x1b[1;37m· \\x1b[38;5;242m· \\x1b[38;5;235m·\\x1b[0m \\x1b[38;5;233m───────────────────────────────────────────\\x1b[38;5;237m░\\x1b[38;5;242m│\\x1b[0m\\n"
+printf " \\x1b[38;5;235m│\\x1b[38;5;237m░░░ \\x1b[38;5;242mcommands\\x1b[38;5;240m: help, sysinfo, rules, vhosts               \\x1b[38;5;237m░\\x1b[38;5;242m│\\x1b[0m\\n"
+printf " \\x1b[38;5;235m└─────────────────────────────────────────────·─\\x1b[38;5;242m─\\x1b[1;39m·─\\x1b[38;5;242m─\\x1b[1;39m─\\x1b[38;5;242m──────\\x1b[38;5;255m┘\\x1b[0m\\n"
 
 # vim: ts=2 sw=2 noet :
