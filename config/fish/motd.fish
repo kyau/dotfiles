0759a67e8437513390511759142f6980015ccdf8
Sean Bruen <kyau@kyau.net>
2020-11-15 23:00:44 -0800

    November 2020 Update
    * Add: ssh hosts to local config
    * Update: fish motd updated to remove unused domains
    * Update: i3 laptop config updated to reflect all kvm hosts
    * Update: polybar font size

--- config/fish/motd.fish
+++ config/fish/motd.fish
@@ -1,9 +1,9 @@
-# $KYAULabs: motd.fish,v 1.3.7 2020/01/20 10:37:33 kyau Exp $
+# $KYAULabs: motd.fish,v 1.3.8 2020/11/13 22:52:14 kyau Exp $
 
 # ANSI
 set -l _fish_hostname (cat /proc/sys/kernel/hostname | cut -d '.' -f 1)
 set -g ANSI $HOME/dot/ansi/$_fish_hostname.ans
-set -g _SSL_DOMAINS "ah42.org" "botpack.xyz" "kyau.net" "kyaulabs.com" "voidbbs.com"
+set -g _SSL_DOMAINS "botpack.xyz" "kyau.net" "kyaulabs.com" "voidbbs.com"
 set -g _SERVICES "nftables" "sshd" "nginx" "mariadb"
 
 # Padding/Remove Color {{{
